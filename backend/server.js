8a32a8bce3bd62ac42e0ab0daa7171a45cf5eed6
MihirKumar <mihirmalik34@gmail.com>
2026-04-17 03:23:52 +0530

    Fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", //Local dev
-      "https://lost-link-1.onrender.com/",
+      "https://lost-link-1.onrender.com",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ const userSocketMap = new Map();
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "https://lost-link-1.onrender.com/"],
+      origin: ["http://localhost:5173", "https://lost-link-1.onrender.com"],
 
       credentials: true,
     },
